4954882b34f7c51b869aaa6a1aa78f5b57eba695
Usman Gill <usmangill854@gmail.com>
2022-01-02 20:32:39 +0500

    fome data

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express() 
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3000
+
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','pug')
+
+app.get('/',(req,res)=>{
+    res.render('index',{title:'Form Handling'})
+})
+
+
+app.listen(5000 , () =>{
+    console.log('server is listening on port 5000')
+})
