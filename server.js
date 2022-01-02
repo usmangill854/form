1eab529d344ae97c358b85af40acc778159c623a
Usman Gill <usmangill854@gmail.com>
2022-01-02 20:55:33 +0500

    complete

--- server.js
+++ server.js
@@ -7,10 +7,18 @@ const PORT = process.env.PORT || 3000
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','pug')
 
+app.use(express.urlencoded({
+}))
+
 app.get('/',(req,res)=>{
     res.render('index',{title:'Form Handling'})
 })
-
+app.post('/form_submit',(req,res)=>{
+    const username=req.body.username
+    const email = req.body.email
+    res.send( `your username is ${username}
+               and your email is ${email}`)
+})
 
 app.listen(5000 , () =>{
     console.log('server is listening on port 5000')
