ef6acc2bca476844732c39044537a61f69bde081
Usman Gill <usmangill854@gmail.com>
2022-01-03 21:38:47 +0500

    cookies

--- server.js
+++ server.js
@@ -4,30 +4,57 @@ const app = express()
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5000
 
-const session = require('express-session')
+const cookies= require('cookie-parser')
 
-app.use(session({
-    secret:'your secret key',
-    resave:true,
-    saveUninitialized:true
-
-}))
+app.use(cookies())
+const user={
+    name:'john',
+    age:24
+}
 app.get('/',(req,res) => {
-    req.session.name = 'john'
-    return res.send('session set')
+    res.send('cookies toturial')
 })
+ app.get('/setuser',(req,res) => {
+     res.cookie('userData',user)
+     res.send('userData added to cookies')
+ })
 
-app.get('/session',(req,res) =>{
-    var name = req.session.name
-    return res.send(name)
-})
+ app.get('/getuser',(req,res) => {
+     res.send(req.cookies)
+ })
+ app.get('/logout',(req,res) => {
+     res.clearCookie('userData')
+     res.send('user logout successfully')
+ })
 
-app.get('/destroy',(req,res)=>{
-    req.session.destroy((err)=>{
-        console.log('session destroyed')
-    })
-    res.end()
-})
+
+// const session = require('express-session')
+
+// app.use(session({
+//     secret:'your secret key',
+//     resave:true,
+//     saveUninitialized:true
+
+// }))
+// app.get('/',(req,res) => {
+//     req.session.name = 'john'
+//     return res.send('session set')
+// })
+
+// app.get('/session',(req,res) =>{
+//     var name = req.session.name
+//     return res.send(name)
+// })
+
+// app.get('/destroy',(req,res)=>{
+//     req.session.destroy((err)=>{
+//         console.log('session destroyed')
+//     })
+//     res.end()
+// })
+
+
+
 
 
 
