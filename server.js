92b02dd9c2a278cede8b269498714af086949dac
Usman Gill <usmangill854@gmail.com>
2022-01-03 19:58:15 +0500

    sessions

--- server.js
+++ server.js
@@ -2,24 +2,51 @@ const express = require('express')
 const path = require('path')
 const app = express() 
 const mongoose = require('mongoose')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
-app.set('views',path.join(__dirname,'views'))
-app.set('view engine','pug')
+const session = require('express-session')
 
-app.use(express.urlencoded({
+app.use(session({
+    secret:'your secret key',
+    resave:true,
+    saveUninitialized:true
+
 }))
-
-app.get('/',(req,res)=>{
-    res.render('index',{title:'Form Handling'})
-})
-app.post('/form_submit',(req,res)=>{
-    const username=req.body.username
-    const email = req.body.email
-    res.send( `your username is ${username}
-               and your email is ${email}`)
+app.get('/',(req,res) => {
+    req.session.name = 'john'
+    return res.send('session set')
 })
 
-app.listen(5000 , () =>{
+app.get('/session',(req,res) =>{
+    var name = req.session.name
+    return res.send(name)
+})
+
+app.get('/destroy',(req,res)=>{
+    req.session.destroy((err)=>{
+        console.log('session destroyed')
+    })
+    res.end()
+})
+
+
+
+// app.set('views',path.join(__dirname,'views'))
+// app.set('view engine','pug')
+
+// app.use(express.urlencoded({
+// }))
+
+// app.get('/',(req,res)=>{
+//     res.render('index',{title:'Form Handling'})
+// })
+// app.post('/form_submit',(req,res)=>{
+//     const username=req.body.username
+//     const email = req.body.email
+//     res.send( `your username is ${username}
+//                and your email is ${email}`)
+// })
+
+app.listen(PORT , () =>{
     console.log('server is listening on port 5000')
 }) 
